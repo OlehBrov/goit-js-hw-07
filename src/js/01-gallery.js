cae22a5f68c63c3697c7080017fad4905036e8e5
OlehBrov <o.dudnyk85@gmail.com>
2023-01-04 20:47:13 +0200

    task-01-complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems.map(({preview, original, description}) => 
+`<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="I${description}"
+  />
+</a>
+</div>`)  
+.join("");
+
+galleryRef.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+galleryRef.addEventListener("click", openOriginalImage);
+
+function openOriginalImage(event){
+  if(event.target.nodeName !== 'IMG') return;
+  event.preventDefault();
+   return basicLightbox.create(`<img src="${event.target.dataset.source}" alt = "${event.target.alt}"/>`, {className: 'basicLightbox'}).show()
+  
+}
